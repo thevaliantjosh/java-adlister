6f52eff843112312f64530ad8c3c541856c0c6e7
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-15 11:31:07 -0600

    First commit

--- src/main/java/ParameterTestServlet.java
+++ src/main/java/ParameterTestServlet.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "ParameterTestServlet", urlPatterns = "/parameterTest")
+public class ParameterTestServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!<h1>");
+        String word = request.getParameter("word");
+        out.println("<h1>You entered " + word + "!<h1>");
     }
 }
